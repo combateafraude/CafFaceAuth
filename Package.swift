e8621cbfb878e48490561dffe56af42bb526d796
marcusseixascaf <150820175+marcusseixascaf@users.noreply.github.com>
2024-12-17 14:58:04 -0300

    Release 6.2.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/combateafraude/FaceLiveness.git",
-            .exact("6.3.0")
+            .exact("6.3.2")
         )
     ],
     targets: [
